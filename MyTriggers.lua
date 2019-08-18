eb802f658ea0aeb470569b9d4708f431e5850e7f
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2019-08-18 20:54:18 +0200

    Bumped version number for 8.2

--- MyTriggers.lua
+++ MyTriggers.lua
@@ -81,7 +81,7 @@ function MyTriggers_DefaultSettings()
     settings.aoeEnemyThreshold = 5
     settings.priWorldBoss = true
     settings.priElite = true
-    settings.bossHealth = 15000000
+    settings.bossHealth = 15000
     settings.inRangeOfAoeSpell = MyTriggers_GetInRangeOfAoeSpell()
 
     settings.isPriority = false
